e7408b9163bcd023c044ecebbdfc42c168e75b07
315933 <17676504725@163.com>
2024-12-07 11:47:46 +0800

    Update WechatConfig.java

--- src/main/java/com/pt/vx/config/WechatConfig.java
+++ src/main/java/com/pt/vx/config/WechatConfig.java
@@ -38,7 +38,7 @@ public class WechatConfig {
      */
     static {
         userList.add(getUser(
-                "oqWn06uuX8UukJTPc-DilnsVZy5c", //扫码关注你的测试号以后，测试平台会出现TA的微信号
+                "oqWn06nDjtBrsJ8NYL3Lnxi3ucxs", //扫码关注你的测试号以后，测试平台会出现TA的微信号
                 "g5aYv0JZviD3bGiaa99aGJ7YVvKBdSlwZuZJkX30gPA", //要给这个人发送的模板ID
                 "猪", //咋称呼这个人
                 "广东省湛江市麻章区", //这个人的详细地址
